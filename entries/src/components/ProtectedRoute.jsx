0a0e72ee4133971731e1b7c5b2d250c6144eb1d4
Olawise001 <olalekanarowolo88@gmail.com>
2026-05-15 21:16:15 +0000

    rename

--- entries/src/components/ProtectedRoute.jsx
+++ entries/src/components/ProtectedRoute.jsx
@@ -7,7 +7,7 @@ const DefaultFallback = () => (
   <div className="fixed inset-0 flex items-center justify-center">
     <div className="w-8 h-8 border-4 border-slate-200 border-t-slate-800 rounded-full animate-spin"></div>
   </div>
-);
+); 
 
 export default function ProtectedRoute({ fallback = <DefaultFallback />, unauthenticatedElement }) {
   const { isAuthenticated, isLoadingAuth, authChecked, authError, checkUserAuth } = useAuth();
